960380a6cbafa46b01dca36e6bbe022759e9bedc
irunwazed <ahmad.arsyad@bkn.go.id>
2023-10-08 09:46:29 +0700

    first commit

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex flex-row h-screen">
+        <div class="basis-1/2 p-20">
+            <div class="text-4xl mb-2">Simulator</div>
+            <div class="font-bold text-6xl mb-16">MANAJEMEN PELABUHAN</div>
+            <div class="text-2xl mb-4">Here to get, <span class="font-semibold">welcome</span></div>
+            <div>
+                <div>
+                    <label for="">Username</label>
+                    <input type="text" name="username">
+                </div>
+            </div>
+            <div>
+                <a href="#">Forget password?</a>
+                <button>Continue</button>
+            </div>
+        </div>
+        <div class="basis-1/2 bg-blue-600 h-auto">
+            Gambar
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome3.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div>
+        <h1>Testong</h1>
+
+        <span class="font-bold bg-red-600">Sya</span>
+    </div>
+</body>
+</html>
