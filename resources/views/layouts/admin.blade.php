d08fa147ba1c7a0ef813fcba97b3e85198f40851
Ahmad Khairun Arsyad <ahmad.arsyad@bkn.go.id>
2023-10-26 20:50:46 +0700

    fixed css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin - @yield('title')</title>
-  @vite('resources/css/app.css')
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+  @vite('resources/css/app.css')
 
 </head>
 
