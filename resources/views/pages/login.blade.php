0305d18c26af7ab74e10d6a646a0be9af306ac2a
Ahmad Khairun Arsyad <ahmad.arsyad@bkn.go.id>
2023-10-27 09:45:05 +0700

    route

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    @vite('resources/css/app.css')
+  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 </head>
 <body>
     <div class="flex flex-row h-screen">
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    header("Location: login");
+    die();
 });
 
 Route::get('/login', function () {
