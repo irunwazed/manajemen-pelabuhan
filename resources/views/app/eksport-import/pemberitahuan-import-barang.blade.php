cca9d65b4254f526860192f744c07ac891f8c73a
Mukhammad Fakhir Rizal <bokir.rizal@gmail.com>
2023-11-25 20:50:07 +0700

    Add pop-up modal in PIB

--- resources/views/app/eksport-import/pemberitahuan-import-barang.blade.php
+++ resources/views/app/eksport-import/pemberitahuan-import-barang.blade.php
@@ -26,13 +26,13 @@
 </div>
 <div class="">
       <div class="text-center mb-3 mt-5">
-        <h2 class="text-2xl font-bold">BC. 2.0 Pemberitahuan Import Barang</h2>
+        <h2 class="text-2xl font-bold">BC 2.0 PEMBERITAHUAN IMPORT BARANG</h2>
         <form action="" method="get">
           <div class="flex flex-wrap place-content-center my-2">
-            <label class="mt-2 mr-2">No Pengajuan : </label>
+            <label class="mt-2 mr-2">No Pengajuan</label>
             <input type="text" name="search" value="{{ @$request['search'] }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 ml-1 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cari</button>
-            <a href="{{url('admin/eksport-import/header')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 ml-1 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Dokumen Baru</a> 
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 ml-1 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">SEARCH</button>
+            <a href="{{url('admin/eksport-import/header')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 ml-1 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">DOKUMEN BARU</a> 
         </div>
         </form>
       </div>
