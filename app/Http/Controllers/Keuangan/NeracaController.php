306a8824a6a860096fe77816239dbc9e208f99f8
Eka Singgih <singgih11@gmail.com>
2023-12-18 17:38:40 +0000

    add filter on journal pages

--- app/Http/Controllers/Keuangan/NeracaController.php
+++ app/Http/Controllers/Keuangan/NeracaController.php
@@ -22,14 +22,24 @@ select rek.kode_rekening, rek.nama_rekening,
        ifnull(sum(jur.saldo), 0) as saldo
 from m_kode_rekening as rek
 left join jurnal as jur on jur.kode_rekening like concat(rek.kode_rekening, '%')
+where rek.kode_rekening like ?
 group by rek.kode_rekening, rek.nama_rekening
 having ifnull(sum(jur.debit), 0) > 0 or ifnull(sum(jur.kredit), 0) > 0
 order by rek.kode_rekening;";
 
     public function index(Request $request)
     {
-        $neraca = DB::select($this->query);
+        $kodeRekening = $request->input('kode_rekening');
+        $neraca = DB::select($this->query, (array) $this->appendPercent($kodeRekening));
 
-        return view('app/keuangan.neraca', compact('neraca'));
+        return view('app/keuangan.neraca', compact('neraca', 'kodeRekening'));
+    }
+
+    private function appendPercent($var)
+    {
+        if (!str_ends_with($var, '%'))
+            return $var . '%';
+
+        return $var;
     }
 }
