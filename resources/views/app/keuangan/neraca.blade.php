1c9fd991d7c9aadc1cc80ee5607bd7d028483855
Eka Singgih <singgih11@gmail.com>
2023-12-18 17:38:40 +0000

    add filter on journal pages

--- resources/views/app/keuangan/neraca.blade.php
+++ resources/views/app/keuangan/neraca.blade.php
@@ -5,6 +5,24 @@
     <div class="">
         <div class="text-2xl ">Neraca</div>
         <hr class="border-b-2 border-black border-solid">
+        <form id="filter-neraca" method="get">
+            <div class="grid grid-cols-4 gap-2 pt-16">
+                <div class="text-start w-full">
+                    <div>
+                        <label>KODE REKENING</label>
+                    </div>
+                    <div>
+                        <input type="text" value="{{$kodeRekening}}" name="kode_rekening" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+                </div>
+                <div class="text-start">
+                    <div>
+                        <button class="text-white mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+                        <button class="text-white mt-6 bg-red-600 hover:bg-blue-800 focus:ring-4 focus:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-600 focus:outline-none dark:focus:bg-red-600" onclick="resetForm()" >Reset</button>
+                    </div>
+                </div>
+            </div>
+        </form>
         <div class="mb-3 mt-5">
             <div>
                 <table class="mt-5 w-full border-solid border-2 border-slate-800">
@@ -32,6 +50,15 @@
             </div>
         </div>
     </div>
+    <script>
+        function resetForm() {
+            Array.prototype.slice.call(
+                document.getElementsByTagName('input'))
+                .forEach(function (el) {
+                    el.value = '';
+                });
+        }
+    </script>
 @endsection
 
 @section('script')
